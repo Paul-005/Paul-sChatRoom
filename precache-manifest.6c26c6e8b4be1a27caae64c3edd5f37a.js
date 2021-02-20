b3d1e7acca99aa7fdec7735ef75da3e3cae0b4a9
Paul-005 <70139728+Paul-005@users.noreply.github.com>
2021-02-20 19:47:59 +0530

    Add files via upload

---
+++ precache-manifest.6c26c6e8b4be1a27caae64c3edd5f37a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ce39299dfa6d6b91a972837005aefa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b096f7f46d9d22a3fa25",
+    "url": "/static/css/main.05ad28b9.chunk.css"
+  },
+  {
+    "revision": "591b5d6825e3bbd930a9",
+    "url": "/static/js/2.2ae1d491.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/static/js/2.2ae1d491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b096f7f46d9d22a3fa25",
+    "url": "/static/js/main.f9164278.chunk.js"
+  },
+  {
+    "revision": "a9221baefb3b74a4b30d",
+    "url": "/static/js/runtime-main.9040a9d7.js"
+  }
+]);
